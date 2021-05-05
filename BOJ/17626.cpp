f6dc43848c4da9cfe176ae74b35592d188029d9e
Tak Ji Won <jiwontak@soongsil.ac.kr>
2021-05-05 18:00:10 +0900

    Four Squares

---
+++ BOJ/17626.cpp
@@ -0,0 +1,17 @@
+// 17626번_Four Squares
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n, dp[50001] = {0, };
+    cin >> n;
+    dp[0] = 0;
+    for(int i=1; i<=n; ++i){
+        dp[i] = i;
+        for(int j=1; j*j<=i; ++j){
+            dp[i] = min(dp[i], dp[i-j*j]+1);
+        }
+    }
+    cout << dp[n];
+    return 0;
+}
