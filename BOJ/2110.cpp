920af4d443c198a7fa8be3d26f7fb9ca0acdeb3c
Tak Ji Won <jiwontak@soongsil.ac.kr>
2021-07-29 22:19:30 +0900

    공유기 설치 (수정)

--- BOJ/2110.cpp
+++ BOJ/2110.cpp
@@ -1,14 +1,15 @@
 // 2110번_공유기 설치
+// 3923번째 재채점 결과 WA, 수정완료
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
-int n, c;
+int n, c, max_d;
 vector<int> pos;
 bool check(int distance){
     int cnt = 1, base = pos[0]; // 최소 1개 공유기는 설치 가능
     for(int i=1; i<n; ++i){
-        // 두 좌표간 차이가 계산한 거리 이상이면 base를 다음 좌표로 옮기고 설치 cnt++
+        // 두 좌표간 차이가 계산한 거리 이상이면 base를 다음 좌표로 옮기고 설치 가능한 공유기 개수++
         if(pos[i]-base >= distance){
             cnt++;
             base = pos[i];
@@ -28,13 +29,16 @@ int main(){
     sort(pos.begin(), pos.end());
     // 최소 거리는1, 최대 거리는 (가장 끝 좌표-첫 좌표)
     int lower = 1, upper = pos[n-1]-pos[0], d;
-    while(lower+1 < upper){
+    while(lower <= upper){
         d = (lower+upper)/2;
         // 현재 거리로 c개 이상 공유기 배치 가능한 경우 거리 늘려서 확인
         // c개 이상 배치 불가능하면 거리를 좁혀서 확인
-        if(check(d)) lower = d;
-        else upper = d;
+        if(check(d)){
+            max_d = max(max_d, d); // 현재 거리로 배치 가능한 경우 최대 거리 갱신
+            lower = d+1;
+        }
+        else upper = d-1;
     }
-    cout << lower; // c개 설치 가능한 거리 값 중 가장 작은 값
+    cout << max_d;
     return 0;
 }
